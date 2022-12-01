9655deed5c4b38445155e890ff62f706d99e94e4
theo_m <61331139+pablitoescabito@users.noreply.github.com>
2022-12-02 00:07:47 +0100

    gdhghukvduk

---
+++ questions.js
@@ -0,0 +1,139 @@
+const quiz = {
+    "Protection": [{
+            "question": "Quel est le seul moyen contraceptif efficace contre les ist ?",
+            "reponses": ["préservatif masc/fem", "stérilet", "pilule", "implant"],
+            "entre": [],
+            "correct": "préservatif masc/fem"
+        },
+        {
+            "question": "Jusqu’à combien de temps après un risque de contamination du VIH peut-on demander à bénéficier d’un traitement d’urgence, aussi appelé TPE (Traitement Post Exposition ) ? ",
+            "reponses": ["24h", "1sem", "2 mois", "48h"],
+            "entre": [],
+            "correct": "48h"
+        },
+    ],
+    "Généralité": [{
+            "question": "Que veut dire IST ?",
+            "reponses": ["institut salariale du travail", "infection sure de la trisomie", "infection sexuellement transmissible", "institut securisée du travail"],
+            "entre": [],
+            "correct": "Infection sexuellement transmissible"
+        },
+        {
+            "question": "Qu'est ce que le VIH ?",
+            "reponses": ["virus de l’immunodéficience humaine", "verranda intérieure hospitalière", "vestibule interne haut", "virus interne humain"],
+            "entre": [],
+            "correct": "Virus de l’Immunodéficience Humaine"
+        },
+        {
+            "question": "Par quel liquides corporels le VIH ne se transmet-il pas ?",
+            "reponses": ["le sang", "le sperme", "la salive", "le lait maternel"],
+            "entre": [],
+            "correct": "la salive"
+        },
+        {
+            "question": "Quel est le virus responsable du sida ?",
+            "reponses": ["le virus de l’immunodéficience humaine", "le virus de la grippe", "le virus de la gastro", "le virus de la bronchiolite"],
+            "entre": [],
+            "correct": "le virus de l’immunodéficience humaine"
+        },
+    ],
+    "Dépistage": [{
+            "question": "Qu'est ce que le CDAG ?",
+            "reponses": ["consultation du dimanche accessible pour les gay", "concentration digitale automatique gavite", "consultation digitale anonyme et gratuite", "consultations de dépistage anonyme et gratuit"],
+            "entre": [],
+            "correct": "consultations de dépistage anonyme et gratuit"
+        },
+        {
+            "question": "Qu'est ce que le VIH ?",
+            "reponses": ["Virus de l’Immunodéficience Humaine", "sida", "test", "test2"],
+            "entre": [],
+            "correct": "Virus de l’Immunodéficience Humaine"
+        },
+        {
+            "question": "Par quel liquides corporels le VIH ne se transmet-il pas ?",
+            "reponses": ["le sang", "le sperme", "la salive", "le lait maternel"],
+            "entre": [],
+            "correct": "la salive"
+        },
+        {
+            "question": "Qu’est ce que le CIDDIST ? ",
+            "reponses": ["centre indicatif des destructions interne sur terre", "centre indicatif des dommages infligés par le sida transmissible", "centre d’interaction pour débile dangereux de saint tropez", "centres d’information de dépistage et de diagnostic des ist"],
+            "entre": [],
+            "correct": "centres d’information de dépistage et de diagnostic des ist"
+        },
+        {
+            "question": "Qu’est ce que le CIDDIST ? ",
+            "reponses": ["interruption volontaire de grossesse", "infection via personnes gentilles", "infection et virus de gay", "institut virale pour gaulois"],
+            "entre": [],
+            "correct": "centres d’information de dépistage et de diagnostic des ist"
+        },
+    ],
+    "Sexualité": [{
+            "question": "Quel type de comportement pourrait m’exposer à des IST ?",
+            "reponses": ["s’appeler jérôme", "boire du jus de pomme", "faire des câlins", "avoir des rapports à risque sans protection"],
+            "entre": [],
+            "correct": "avoir des rapports à risque sans protection"
+        },
+        {
+            "question": "Récemment, j’ai eu un rapport non protégé et je présente des symptômes. Que dois-je faire ?",
+            "reponses": ["regarder la télé", "attendre que ca passe", "en parler avec un professionnel de santé", "ne rien faire"],
+            "entre": [],
+            "correct": "en parler avec un professionnel de santé"
+        },
+    ],
+        "Chiffres clés": [{
+            "question": "Combien pensez-vous que de personnes sont atteintes du Sida dans le monde ?",
+            "reponses": ["384 000", "38,4 000", "38,4 millions", "3"],
+            "entre": [],
+            "correct": "38,4 millions"
+        },
+        {
+            "question": "Quel est le pays qui recense le plus de cas de Sida ?",
+            "reponses": ["l’afrique du sud", "l’afrique du nord", "l'australie", "russie"],
+            "entre": [],
+            "correct": "l’afrique du sud"
+        },
+        {
+            "question": "Combien de cas de séropositivité VIH sont recensés chaque année environ ?",
+            "reponses": ["655 000", "1 500 000", "350 000", "180 000"],
+            "entre": [],
+            "correct": "1 500 000"
+        },    
+        {
+            "question": "Combien de décès liés au VIH en 2021 ?",
+            "reponses": ["320 000", "650 000", "170 000", "1 000 000"],
+            "entre": [],
+            "correct": "650 000"
+        },    
+        {
+            "question": "Combien de personnes ont  accès au traitement du VIH en 2021 ?",
+            "reponses": ["1 500 000 000", "80 000 000", "28 700 000", "10 000 000"],
+            "entre": [],
+            "correct": "28 700 000"
+        },    
+        {
+            "question": "Combien de personnes infectées depuis le début de l'épidémie ?",
+            "reponses": ["45 000 000", "166 000 000", "555 000 000", "84 200 000"],
+            "entre": [],
+            "correct": "84 200 000"
+        },
+        {
+            "question": "Quel est le type de personne qui vit le plus avec le VIH ?",
+            "reponses": ["femme/fille  avec 54%", "personnes trans/gay avec 66%", "garçons avec 60%", "personnes âgées 61%"],
+            "entre": [],
+            "correct": "femme/fille  avec 54%"
+        },
+        {
+            "question": "Combien y’a t-il d'enfants vivant avec le VIH dans le monde ?",
+            "reponses": ["3 600 000", "1 700 000", "6 500 000", "12 500 000"],
+            "entre": [],
+            "correct": "1 700 000"
+        },
+        {
+            "question": "Combien y avait-il de personnes qui ne savaient pas qu’elles avaient le VIH en 2021 ?",
+            "reponses": ["150 000", "1 000 000", "1 700 000", "10 000 000"],
+            "entre": [],
+            "correct": "1 700 000"
+        },
+],
+}
