418b5c33ba2722a99ff213fc683f48363bab13f8
theo_m <61331139+pablitoescabito@users.noreply.github.com>
2022-12-02 01:29:34 +0100

    khuir

--- questions.js
+++ questions.js
@@ -11,6 +11,13 @@ const quiz = {
             "entre": [],
             "correct": "48h"
         },
+        {
+            "question": "Il existe des vaccins pour",
+            "reponses": ["La calvitie et la toux", "L’hépatite B et le papillomavirus", "L’hépatite C et le VIH", "Herpès génital et la syphilis"],
+            "entre": [],
+            "correct":  "L’hépatite B et le papillomavirus"
+        },
+
     ],
     "Généralité": [{
             "question": "Que veut dire IST ?",
@@ -36,6 +43,13 @@ const quiz = {
             "entre": [],
             "correct": "le virus de l’immunodéficience humaine"
         },
+        {
+            "question": "Qu’est ce que la sérophobie",
+            "reponses": ["La peur des seringues", "La discriminations des personnes séropositives", "Quelqu’un qui aime le sang", "Quelqu’un qui a peur des gens sérieux"],
+            "entre": [],
+            "correct": "La discriminations des personnes séropositives"
+        },
+
     ],
     "Dépistage": [{
             "question": "Qu'est ce que le CDAG ?",
@@ -73,6 +87,61 @@ const quiz = {
             "entre": [],
             "correct": "interruption volontaire de grossesse"
         },
+        {
+            "question": "Quel métier n’est pas interdit aux personnes séropositives",
+            "reponses": ["Sapeur-Pompier", "Gerndarme", "Professeur des écoles", "Militaire"],
+            "entre": [],
+            "correct": "Professeur des écoles"
+        },
+        {
+            "question": "Ou ne peut-on pas se faire dépister",
+            "reponses": ["CHU", "Laboratoires", "Centes de plannification", "Au collège  / au lycée"],
+            "entre": [],
+            "correct": "Au collège  / au lycée"
+        },
+     
+        {
+            "question": "Les MST sont-elles toujours visibles",
+            "reponses": ["Oui, il y a toujours des signes", "Non, il n’y a jamais de signes", "Il y a des signes les week-ends", "Souvent il n’y a pas de signes"],
+            "entre": [],
+            "correct":  "Souvent il n’y a pas de signes"
+        },
+           
+        { "question": "Le papillomavirus peut provoquer",
+           "reponses": ["Des cancers", "Le sida", "Un chancre", "De multiples spasmes"],
+            "entre": [],
+            "correct":  "Souvent il n’y a pas de signes"
+        },
+        { "question": "Quelle est l’IST la plus répandue chez les jeunes",
+            "reponses": ["La gonorrhée", "La syphilis", "La chlamydia", "Le VIH"],
+            "entre": [],
+            "correct":  "La chlamydia"
+        },
+        {
+            "question": "Quelle est le meilleur moyen de savoir si tu as une IST",
+            "reponses": ["Regarde ton sexe", "Fais des recherches sur internet", "Impossible de le savoir", "Dépistage"],
+            "entre": [],
+            "correct":  "Dépistage"
+        },
+        {
+            "question": "Combien de temps dois-je attendre pour faire un dépistage du VIH par prise de sang après une prise de risque ?",
+            "reponses": ["1 jour", "6 semaines", "3 mois", "2 ans"],
+            "entre": [],
+            "correct":  "6 semaines"
+        },
+        {
+            "question": "Une personne séropositive qui a une charge virale indétectable",
+            "reponses": ["N’a plus le VIH dans son organisme", "Est devenu aveugle", "N’a presque plus de VIH dans son organisme", "A encore autant de VIH dans son organisme"],
+            "entre": [],
+            "correct":  "N’a presque plus de VIH dans son organisme"
+        },
+        {
+            "question": "Être infecté par une IST augmente le risque d’être par le VIH",
+            "reponses": ["Faux, on ne peut avoir qu’une seule IST", "Vrai", "Faux, être infecté par une IST n’augmente pas le risque d’avoir le VIH", "Faux une IST augmente ses défenses face au VIH"],
+            "entre": [],
+            "correct":  "Vrai"
+        },
+
     ],
     "Sexualité": [{
             "question": "Quel type de comportement pourrait m’exposer à des IST ?",
@@ -86,6 +155,31 @@ const quiz = {
             "entre": [],
             "correct": "en parler avec un professionnel de santé"
         },
+        {
+            "question": "Quel rapport n’est pas contaminant",
+            "reponses": ["Rapport anal", "Rapport vaginal", "Féllation", "Cunnilingus"],
+            "entre": [],
+            "correct": "Cunnilingus"
+        },
+        {
+            "question": "Le risque de transmission d’une IST en pratiquant une fellation est ",
+            "reponses": ["Faible", "Moyen", "Elevé", "Très élevé"],
+            "entre": [],
+            "correct": "Très élevé"
+        },
+        {
+            "question": "La fellation sans préservatif peut-elle représenter un risque de transmission d’une IST",
+            "reponses": ["Oui", "Jamais", "Non sauf si il y a éjaculation", "Une chance sur 2"],
+            "entre": [],
+            "correct":  "Oui"
+        },
+        {
+            "question": "Qu’est ce qu’un personne séropositive",
+            "reponses": ["Quelqu’un du groupe O+", "Quelqu’un qui a le SIDA", "Quelqu’un qui est porteur du VIH", "Quelqu’un qui est joyeux"],
+            "entre": [],
+            "correct": "Quelqu’un qui a le SIDA"
+        },
+
     ],
     "Chiffres clés": [{
             "question": "Combien pensez-vous que de personnes sont atteintes du Sida dans le monde ?",
@@ -139,7 +233,64 @@ const quiz = {
             "question": "Combien y avait-il de personnes qui ne savaient pas qu’elles avaient le VIH en 2021 ?",
             "reponses": ["150 000", "1 000 000", "1 700 000", "10 000 000"],
             "entre": [],
-            "correct": "1 700 000"
+            "correct": "1 700 000" 
         },
-],
+        {
+            "question": "Combien de personnes sont diagnostiquées séropostives par jour en France ?",
+            "reponses": ["7", "9", "13", "24"],
+            "entre": [],
+            "correct":  "13"
+        },
+
+    ],
+    "Contamination": [{
+            "question": "Quel type de comportement pourrait m’exposer à des IST ?",
+            "reponses": ["s’appeler jérôme", "boire du jus de pomme", "faire des câlins", "avoir des rapports à risque sans protection"],
+            "entre": [],
+            "correct": "avoir des rapports à risque sans protection"
+        },
+        {
+            "question": "Récemment, j’ai eu un rapport non protégé et je présente des symptômes. Que dois-je faire ?",
+            "reponses": ["regarder la télé", "attendre que ca passe", "en parler avec un professionnel de santé", "ne rien faire"],
+            "entre": [],
+            "correct": "en parler avec un professionnel de santé"
+        },
+        {
+            "question": "Est-ce qu’une IST, c’est signe de mauvaise hygiène",
+            "reponses": ["Oui", "Non pas du tout", "Cela dépend de la personne", "Cela dépend de l’IST"],
+            "entre": [],
+            "correct":  "Non pas du tout"
+        },
+        {
+            "question": "Quelles IST peuvent-elles transmettre par les pailles de sniff",
+            "reponses": ["L’hépatite B et C", "Le papillomavirus et le VIH", "La syphilis et la chlamydioses", "Il n’y a aucun risque"],
+            "entre": [],
+            "correct":  "L’hépatite B et C"
+        },
+        {
+            "question": "Le symptôme principal de l’herpès",
+            "reponses": ["Agrandissement du penis", "Déformation des oreilles", "Une diarrhée", "Des vésicules sur les organes sexuels"],
+            "entre": [],
+            "correct":  "Des vésicules sur les organes sexuels"
+        },
+        {
+            "question": "Le VIH s’attaque au",
+            "reponses": ["Système immunitaire", "Système reproducteur", "Système digestif", "Système cognitif"],
+            "entre": [],
+            "correct":  "Système immunitaire"
+        },
+        {
+            "question": "Quelle méthode ne protège pas du VIG",
+            "reponses": ["Les préservatifs", "Le dépistage", "Les traitements", "la pillule"],
+            "entre": [],
+            "correct": "la pillule"
+        },
+        {
+            "question": "Laquelle de ces propositions est vraie",
+            "reponses": ["Le virus du sida se transmet entre autre par le sperme, les sécrétions vaginales et le liquide séminal", "Le virus du sida se transmet uniquement par le sang", "Le virus du sida se transmet par la salive", "Le virus du sida se transmet par télépathie "],
+            "entre": [],
+            "correct":  "Le virus du sida se transmet entre autre par le sperme, les sécrétions vaginales et le liquide séminal"
+        },
+
+    ],
 }
